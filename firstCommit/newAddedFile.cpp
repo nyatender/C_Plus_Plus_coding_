b318fdd8c084b17af9e6e374441d7d191aa8dfc1
yatender <negi.yatenn@gmail.com>
2019-08-26 22:48:39 +0530

    bug [5050]: git changes

---
+++ firstCommit/newAddedFile.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "first proj" << endl;
+	cout << "created a new branch Algorith" << endl;
+	
+	cout << "created a new branch Algorith" << endl;
+
+	getchar();
+	return 0;
+}
